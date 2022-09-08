833eb40acc793d1de5eaad99574cb455ca43e4a1
Nicolas Gonzalez <ndgfrs@gmail.com>
2022-09-08 02:15:55 -0400

    Testing assisted development using Github Copilot.

--- src/format.c
+++ src/format.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdarg.h>
 
 static int64_t multiply_by_10_to_the_nth(int64_t value, int8_t n) {
   while (n-- > 0) {
@@ -59,7 +60,7 @@ int64_t str_len(char *str) {
   return length;
 }
 
-static char * float_to_str(float value, int8_t precision) {
+static char * double_to_str(float value, int8_t precision) {
   bool is_negative = false;
   if (value < 0) {
     is_negative += true;
@@ -70,7 +71,8 @@ static char * float_to_str(float value, int8_t precision) {
   // Convert the left side of the decimal into a string.
   char *lvalue = int_to_str((int64_t) value);
   // "rvalue" refers to the right side of the decimal.
-  bool has_rvalue = (((float) (value - (int64_t) value)) != 0);
+  // bool has_rvalue = (((float) (value - (int64_t) value)) != 0);
+  bool has_rvalue = (precision > 0);
   char *rvalue = NULL;
   if (has_rvalue) {
     rvalue = int_to_str(
@@ -91,7 +93,7 @@ static char * float_to_str(float value, int8_t precision) {
   );
 
   char *result = (char *) calloc(result_length, sizeof(char));
-  int64_t index = 0, jndex = 0;  // For incrementing dest and source.
+  int64_t index = 0, j = 0;  // For incrementing dest and source.
 
   // If `value` is negative, append a negative sign.
   if (is_negative) {
@@ -99,7 +101,7 @@ static char * float_to_str(float value, int8_t precision) {
   }
 
   // Append the lvalue.
-  while ((result[index] = lvalue[jndex++])) {
+  while ((result[index] = lvalue[j++])) {
     index++;
   }
 
@@ -109,9 +111,9 @@ static char * float_to_str(float value, int8_t precision) {
     while (precision-- > str_len(rvalue)) {
       result[index++] = '0';
     }
-    jndex = 0;
+    j = 0;
     // Append the right value to `result`.
-    while ((result[index++] = rvalue[jndex++])) {}
+    while ((result[index++] = rvalue[j++])) {}
   }
 
   // Separated left and right values are no longer needed;
@@ -122,16 +124,133 @@ static char * float_to_str(float value, int8_t precision) {
   return result;
 }
 
-// char * format(char **str, const char *fmt, ...) {}
+// printf-style string formatting (This function was mostly written by Github Copilot.)
+// I need to go back and redo it with a better understanding of how it works.
+char * format(char **str, const char *fmt, ...) {
+  int64_t str_size = str_len((char *) fmt);
+  *str = (char *) calloc(str_size, sizeof(char));
+  int64_t index = 0;
+  va_list args;
+  va_start(args, fmt);
+  int64_t precision = 6;
+  char *src = (char *) fmt;
+  while (*src != '\0') {
+    if (*src != '%') {
+      (*str)[index++] = *src++;
+      continue;
+    }
+    // If the next character is a '%', find the format specifier.
+    else {
+      if (*(src + 1) == '%') {
+        (*str)[index++] = *src++;
+        continue;
+      }
+      else if (*(src + 1) == 'd') {
+        char *value = int_to_str(va_arg(args, int64_t));
+        int64_t j = 0;
+        while ((value[j] != '\0')) {
+          (*str)[index++] = value[j++];
+        }
+        free(value);
+        src += 2;
+        continue;
+      }
+      else if (*(src + 1) == 'f') {
+        char *value = double_to_str(va_arg(args, double), precision);
+        int64_t j = 0;
+        while ((value[j] != '\0')) {
+          (*str)[index++] = value[j++];
+        }
+        free(value);
+        src += 2;
+        continue;
+      }
+      else if (*(src + 1) == 's') {
+        char *value = va_arg(args, char *);
+        int64_t j = 0;
+        while ((value[j] != '\0')) {
+          (*str)[index++] = value[j++];
+        }
+        src += 2;
+        continue;
+      }
+      else if (*(src + 1) == 'c') {
+        (*str)[index++] = va_arg(args, int64_t);
+        src += 2;
+        continue;
+      }
+      // else if (*(src + 1) == '.') {
+      //   if ((*(src + 2) >= '0') || (*(src + 2) <= 9)) {
+      //     precision = *(src + 2) - '0';
+      //     char *value = double_to_str(va_arg(args, double), precision);
+      //     int64_t j = 0;
+      //     while ((value[j] != '\0')) {
+      //       (*str)[index++] = value[j++];
+      //     }
+      //     free(value);
+      //     src += 4;
+      //   }
+      //   else {
+      //     (*str)[index++] = *src++;
+      //   }
+      // }
+      else {
+        (*str)[index++] = *src++;
+        continue;
+      }
+    }
+  }
+  return *str;
+}
 
 int32_t main(int32_t argc, const char **argv) {
-  const int32_t favorite_number = -7;
-  char *string = int_to_str(favorite_number);
-  printf("Result: '%s'\n", string);
-  free(string);
-  // char *fmt = format(&format, "Hello, %s!", "World");
-
-  string = float_to_str(-7.023, set_precision(20));
-  printf("float: '%s'\n", string);
+  char *fmt = format(&fmt, "Hello, %s! My favorite number is %f.", "World", 7.0702);
+  printf("fmt: %s\n", fmt);
   return 0;
 }
+
+/**
+ * TODO:
+ * Fix the following:
+ *  - [ ] `format()` does not handle the case where the format specifier is
+ *    - [ ] Add support for %c.
+ *    - [ ] Add support for %d.
+ *    - [ ] Add support for %f.
+ *    - [ ] Add support for %s.
+ *    - [ ] Add support for %x.
+ *    - [ ] Add support for %o.
+ *    - [ ] Add support for %b.
+ *    - [ ] Add support for %p.
+ *    - [ ] Add support for %e.
+ *    - [ ] Add support for %g.
+ *    - [ ] Add support for %n.
+ *    - [ ] Add support for %u.
+ *    - [ ] Add support for %.*f.
+ *    - [ ] Add support for %*.*f.
+ *    - [ ] Add support for %*f.
+ *    - [ ] Add support for %*.*s.
+ *    - [ ] Add support for %*s.
+ *    - [ ] Add support for %*.*d.
+ *    - [ ] Add support for %*d.
+ *    - [ ] Add support for %*.*x.
+ *    - [ ] Add support for %*x.
+ *    - [ ] Add support for %*.*o.
+ *    - [ ] Add support for %*o.
+ *    - [ ] Add support for %*.*b.
+ *    - [ ] Add support for %*b.
+ *    - [ ] Add support for %*.*e.
+ *    - [ ] Add support for %*e.
+ *    - [ ] Add support for %*.*g.
+ *    - [ ] Add support for %*g.
+ *    - [ ] Add support for %*.*n.
+ *    - [ ] Add support for %*n.
+ *    - [ ] Add support for %*.*u.
+ *    - [ ] Add support for %*u.
+ *    - [ ] Add support for %*.*p.
+ *    - [ ] Add support for %*p.
+ *    - [ ] Add support for %*.*c.
+ *    - [ ] Add support for %*c.
+ * 
+ * Fix double_to_str()'s incorrect rvalue.
+ * Find random symbol at after final character of fmt.
+ */
